15c7b0e0b47e1e34f48298a1e6257af488edefec
riverandeye <riverandeye@gmail.com>
2020-09-29 00:49:52 +0900

    .

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import first from "./sample/first";
 import second from "./sample/second";
 import KeyboardMoving from "./sample/keyboard-moving";
+import importBlender from "./sample/import-blender";
+import KeyboardMovingTwo from "./sample/keyboard-moving-2";
 
-window.onload = KeyboardMoving;
+window.onload = KeyboardMovingTwo;
