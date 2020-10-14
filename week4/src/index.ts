6ceb6bfc76751a4f6a93f09c4d02e7321bbbd008
riverandeye <riverandeye@gmail.com>
2020-10-14 17:17:02 +0900

    smile though your heart is aching

---
+++ week4/src/index.ts
@@ -0,0 +1,8 @@
+import first from "./sample/first";
+import second from "./sample/second";
+import KeyboardMoving from "./sample/keyboard-moving";
+import importBlender from "./sample/import-blender";
+import KeyboardMovingTwo from "./sample/keyboard-moving-2";
+import ObjectOriented from "./sample/object-oriented";
+
+window.onload = ObjectOriented;
---
+++ week4/src/sample/object-oriented/index.ts
@@ -0,0 +1,90 @@
+import * as three from "three";
+import * as dat from "dat.gui";
+
+import { addOutputDivOnBody } from "../../libs/dom";
+
+const ObjectOrientedExample = () => {
+  addOutputDivOnBody();
+
+  const scene = new three.Scene();
+  const renderer = new three.WebGLRenderer();
+  const gui = new dat.GUI();
+
+  const axis = new three.AxesHelper(100);
+  scene.add(axis);
+
+  // const light = new three.AmbientLight();
+  const light = new three.PointLight(0x957388, 2);
+  light.position.set(0, 40, 0);
+  scene.add(light);
+
+  renderer.setClearColor(0xeeeeee);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.shadowMap.enabled = true;
+  renderer.physicallyCorrectLights = true;
+
+  const camera = new three.PerspectiveCamera(
+    45,
+    window.innerWidth / window.innerHeight,
+    0.01,
+    1000,
+  );
+  camera.position.set(200, 200, 200);
+  camera.lookAt(scene.position);
+
+  //smile
+  document.getElementById("output").appendChild(renderer.domElement);
+
+  const movingBox = new three.Mesh(
+    new three.BoxGeometry(10, 10, 10),
+    new three.MeshLambertMaterial({ color: 0x7295a4 }),
+  );
+
+  const movingBoxAxes = new three.AxesHelper(20);
+  movingBox.add(movingBoxAxes);
+
+  movingBox.position.set(5, 5, 5);
+
+  scene.add(movingBox);
+  renderer.render(scene, camera);
+
+  const currentSpeed = {
+    ArrowLeft: 0,
+    ArrowRight: 0,
+    ArrowUp: 0,
+    ArrowDown: 0,
+  };
+
+  const updatePosition = () => {
+    if (!Object.keys(currentSpeed).every((key) => currentSpeed[key] === 0)) {
+      //
+      const { ArrowLeft, ArrowRight, ArrowUp, ArrowDown } = currentSpeed;
+      movingBox.translateX(ArrowRight - ArrowLeft);
+      movingBox.translateZ(ArrowDown - ArrowUp);
+
+      renderer.render(scene, camera);
+    }
+
+    requestAnimationFrame(updatePosition);
+  };
+
+  updatePosition();
+
+  const updateMovingEvent = (direction: string, keydown: boolean) => {
+    if (keydown) {
+      currentSpeed[direction] = 1;
+    } else {
+      currentSpeed[direction] = 0;
+    }
+  };
+
+  window.addEventListener("keydown", (e: KeyboardEvent) => {
+    updateMovingEvent(e.key, true);
+  });
+
+  window.addEventListener("keyup", (e: KeyboardEvent) => {
+    updateMovingEvent(e.key, false);
+  });
+};
+
+export default ObjectOrientedExample;
